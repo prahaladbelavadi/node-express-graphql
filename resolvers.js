6ef3e0171a5c10c4bcb4f94f930011b42d2b827c
Prahalad Belavadi <github@belavadi.com>
2021-12-10 07:12:16 +0530

    handle resolvers for students

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,8 @@
+const db = require( "./db" );
+const { students } = require( "./db" );
+
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!'
+    test: () => 'Test Success, GraphQL server is up & running !!',
+    students: () => db.students.list()
 };
 module.exports = { Query };
